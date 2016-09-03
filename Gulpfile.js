d7950ab13c22afe0010535fb8e59e2abac81ccee
fahrbier <holger@lersch.net>
2016-09-03 14:40:00 -0400

    data classes rendered using gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var insert = require('gulp-insert');
-
+var mustache = require("gulp-mustache");
+var rename = require("gulp-rename");
 
 gulp.task('default', function() {
 
@@ -10,4 +11,61 @@ gulp.task('default', function() {
         './services/*service.inc',
     ]).pipe(concat('/services.inc')).pipe(insert.wrap('<%', '%>')).pipe(gulp.dest('./dist/'));
 
+});
+
+var dateNow = new Date();
+ 
+gulp.task('writedataclasses', function() {
+    
+    nameClass = "Person";    
+    gulp.src("data/template/dataClass.inc")
+        .pipe( mustache( 
+                         { 
+                             "todaysDate" : dateNow.getFullYear() + '-' + (dateNow.getMonth()+1) + '-' + dateNow.getDate(), 
+                             "name" : nameClass, 
+                             "properties" : [ 
+                                 {"name": "id",       "ccname": "Id",       "type": "string",  "last":false}, 
+                                 {"name": "forename", "ccname": "Forename", "type": "string",  "last":false}, 
+                                 {"name": "surname",  "ccname": "Surname",  "type": "string",  "last":false}, 
+                                 {"name": "age",      "ccname": "Age",      "type": "integer", "last":true} 
+                             ]
+                         } 
+        ))
+        .pipe(rename('data'+ nameClass +'.inc'))
+        .pipe( gulp.dest('data') );        
+
+    nameClass = "Landmark";            
+    gulp.src("data/template/dataClass.inc")
+        .pipe( mustache( 
+                         { 
+                             "todaysDate" : dateNow.getFullYear() + '-' + (dateNow.getMonth()+1) + '-' + dateNow.getDate(), 
+                             "name" : nameClass, 
+                             "properties" : [ 
+                                 {"name": "id",        "ccname": "Id",        "type": "string",  "last":false}, 
+                                 {"name": "name",      "ccname": "Name",      "type": "real",    "last":false}, 
+                                 {"name": "longitude", "ccname": "Longitude", "type": "real",    "last":false}, 
+                                 {"name": "latitude",  "ccname": "Latitude",  "type": "integer", "last":true} 
+                             ]
+                         } 
+        ))
+        .pipe(rename('data'+ nameClass +'.inc'))
+        .pipe( gulp.dest('data') );    
+
+    nameClass = "News";            
+    gulp.src("data/template/dataClass.inc")
+        .pipe( mustache( 
+                         { 
+                             "todaysDate" : dateNow.getFullYear() + '-' + (dateNow.getMonth()+1) + '-' + dateNow.getDate(), 
+                             "name" : nameClass, 
+                             "properties" : [ 
+                                 {"name": "id",       "ccname": "Id",       "type": "string", "last":false}, 
+                                 {"name": "headline", "ccname": "Headline", "type": "string", "last":false}, 
+                                 {"name": "body",     "ccname": "Body",     "type": "string", "last":true}
+                             ]
+                         } 
+        ))
+        .pipe(rename('data'+ nameClass +'.inc'))
+        .pipe( gulp.dest('data') );    
+
+        
 });
--- dist/index.asp
+++ dist/index.asp
@@ -3,10 +3,18 @@
 set ls = new LandmarkListService
 ls.LandmarkListService("")
 
+set ps = new PersonService
+ps.PersonService("")
+
+
 %>
+
+<%= ps.data.Forename() %> - <%= ps.data.Surname() %>
+<%= ps.data.toJson() %>
+
 <ul>
-    <li><%= ls.data(0).getName() %><br/><%= ls.data(0).toJson() %></li>
-    <li><%= ls.data(1).getName() %><br/><%= ls.data(1).toJson() %></li>
+    <li><%= ls.data(0).Name() %><br/><%= ls.data(0).toJson() %></li>
+    <li><%= ls.data(1).Name() %><br/><%= ls.data(1).toJson() %></li>
 </ul>
 
 <%= ls.jsonArray %>    
